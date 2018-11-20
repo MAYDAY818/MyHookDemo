d78160ad7b71e8065986d02d26db902db7bfd2e9
MAYDAY818 <xiaojingfei818@gmail.com>
2018-11-20 15:23:44 +0800

    Initial commit

---
+++ app/src/main/java/hook/HookMain.java
@@ -0,0 +1,24 @@
+package hook;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class HookMain implements IXposedHookLoadPackage {
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) {
+        XposedHelpers.findAndHookMethod("com.myhookdemo.MainActivity", loadPackageParam.classLoader, "MyToast", String.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                super.beforeHookedMethod(param);
+                param.args[0] = "Hook成功了";
+            }
+
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                super.afterHookedMethod(param);
+            }
+        });
+    }
+}
